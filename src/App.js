7779f46564c2c6c4de7d1c994312124c8314017c
Atsushi Ishida <gipcompany@gmail.com>
2018-03-08 03:07:30 +0900

    create my own React boilerplate

--- src/App.js
+++ src/App.js
@@ -1,20 +1,8 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Hello, world!</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div><h1>Hello, world!</h1></div>;
   }
 }
 
