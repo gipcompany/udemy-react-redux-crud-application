a4af9e0b3d739bc32241f2bbaf5777f037270879
Atsushi Ishida <gipcompany@gmail.com>
2018-03-10 13:52:50 +0900

    learn jsx syntax

--- src/App.js
+++ src/App.js
@@ -2,7 +2,12 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, world!</h1>;
+    return (
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onChange={() => {console.log("I am clicked!")}} />
+      </React.Fragment>
+    )
   }
 }
 
