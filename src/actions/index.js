788a3b2968c7e209e6f7f22740de465c89a6794e
Atsushi Ishida <gipcompany@gmail.com>
2018-04-04 02:04:00 +0900

    implement action and action creator

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+export const INCREMENT = 'INCREMENT'
+export const DECREMENT = 'DECREMENT'
+
+export const increment = () => ({
+  type: INCREMENT
+})
+
+export const decrement = () => ({
+  type: DECREMENT
+})
