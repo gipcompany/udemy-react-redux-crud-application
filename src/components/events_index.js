4cfff7bcd15540fe50600277b67288e9094d1703
Atsushi Ishida <gipcompany@gmail.com>
2018-04-29 17:09:07 +0900

    thank you message

--- src/components/events_index.js
+++ src/components/events_index.js
@@ -10,6 +10,7 @@ import {
   TableRow,
   TableRowColumn
 } from 'material-ui/Table'
+import Dialog from 'material-ui/Dialog';
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 import ContentAdd from 'material-ui/svg-icons/content/add'
 
@@ -34,17 +35,75 @@ class EventsIndex extends Component {
     ))
   }
 
+  state = {
+    open: false,
+  };
+
+  handleOpen = () => {
+    this.setState({open: true});
+  };
+
   render() {
     const style = {
       position: "fixed",
       right: 12,
       bottom: 12
     }
+    const signature = {
+      textAlign: 'right'
+    }
+    const title = {
+      fontSize: 27,
+      textAlign: 'right'
+    }
+
     return (
       <React.Fragment>
         <FloatingActionButton style={style} containerElement={<Link to="/events/new" />}>
           <ContentAdd />
         </FloatingActionButton>
+
+        <FloatingActionButton secondary={true} style={{ position: "fixed", right: 128, bottom: 12 }} onClick={this.handleOpen} >
+          <ContentAdd />
+        </FloatingActionButton>
+
+        <Dialog
+          title="本当に有難うございました！またお会いしましょう！"
+          modal={true}
+          contentStyle={{
+            width: '100%',
+            maxWidth: 'none',
+          }}
+          titleStyle={{
+            textAlign: 'center',
+            paddingTop: 100,
+            fontSize: 63,
+          }}
+          style={{
+            padding: 30,
+          }}
+          overlayStyle={{
+            // margin: 30,
+          }}
+          bodyStyle={{
+            margin: 30,
+            paddingTop: 100
+          }}
+          open={this.state.open}
+        >
+          <div style={title}>
+            Reactアプリケーション開発入門
+          </div>
+          <div style={signature}>
+            Udemy プログラミング講師
+          </div>
+          <div style={signature}>
+            石田 敦志 (Atsushi Ishida)
+          </div>
+          <div style={signature}>
+            Happy Coding!
+          </div>
+        </Dialog>
 
         <Table>
           <TableHeader
