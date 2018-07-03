e2fda61657f801e4ae10820fcb991f691b40c293
Atsushi Ishida <gipcompany@gmail.com>
2018-07-04 01:28:33 +0900

    https://reactjs.org/docs/handling-events.html にて紹介されているonClickによるコールバックメソッドの書き方の紹介です。

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ class Counter extends Component {
   constructor(props) {
     super(props)
     this.state = { count: 0 }
+    this.handlePlusButton2 = this.handlePlusButton2.bind(this)
   }
 
   handlePlusButton = () => {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handlePlusButton2() {
+    this.setState({ count: this.state.count + 1 })
+  }
+
+  handlePlusButton3() {
     this.setState({ count: this.state.count + 1 })
   }
 
@@ -21,6 +30,8 @@ class Counter extends Component {
       <React.Fragment>
         <div>count: { this.state.count }</div>
         <button onClick={this.handlePlusButton}>+1</button>
+        <button onClick={this.handlePlusButton2}>+1</button>
+        <button onClick={() => {this.handlePlusButton3()}}>+1</button>
         <button onClick={this.handleMinusButton}>-1</button>
       </React.Fragment>
     )
