c80e03e12206d171948274f69ccbd9cc705b360c
Atsushi Ishida <gipcompany@gmail.com>
2018-10-09 00:00:06 +0900

    https://www.udemy.com/react-application-development/learn/v4/questions/5323376
    にて、受講生様より頂いたソースコード(`events_new.js`)をbranch `events-new` に適用したもの。

--- src/components/events_new.js
+++ src/components/events_new.js
@@ -1,60 +1,39 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form'
+import { Field, reduxForm} from 'redux-form'
 import { Link } from 'react-router-dom'
 
-import { postEvent } from '../actions'
+//import { postEvents } from '../actions'
 
 class EventsNew extends Component {
-  constructor(props) {
-    super(props)
-    this.onSubmit = this.onSubmit.bind(this)
+  renderField(field){
+    const { input, label, type, meta: { touched, error} } = field
+    return (<div></div>)
   }
-
-  renderField(field) {
-    const { input, label, type, meta: { touched, error } } = field
-
-    return (
-      <div>
-        <input {...input} placeholder={label} type={type} />
-        {touched && error && <span>{error}</span>}
-      </div>
-    )
-  }
-
-  async onSubmit(values) {
-    await this.props.postEvent(values)
-    this.props.history.push('/')
-  }
-
   render() {
-    const { handleSubmit, pristine, submitting } = this.props
-
     return (
-      <form onSubmit={handleSubmit(this.onSubmit)}>
-        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
-        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
-
+      <form>
         <div>
-          <input type="submit" value="Submit" disabled={pristine || submitting} />
-          <Link to="/" >Cancel</Link>
+        <Field label="Title" name="title" type="text" component={this.renderField}/>
+        </div>
+        <div>
+        <Field label="Body" name="body" type="text" component={this.renderField}/>
+        </div>
+        <div>
+          <input type="submit" value="Submit" disabled={false} />
+          <Link to="/">Cancel</Link>
         </div>
       </form>
     )
-  }
+ }
 }
 
 const validate = values => {
   const errors = {}
-
-  if (!values.title) errors.title = "Enter a title, please."
-  if (!values.body) errors.body = "Enter a body, please."
-
   return errors
 }
+//const mapDispatchToProps = ({ postEvent })
 
-const mapDispatchToProps = ({ postEvent })
-
-export default connect(null, mapDispatchToProps)(
-  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+export default connect(null,null)(
+  reduxForm({ validate, form: 'eventNewForm' })( EventsNew)
 )
