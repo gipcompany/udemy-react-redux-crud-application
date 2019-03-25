1130df74ff17af99077fd1684072207ad46cabe3
Atsushi Ishida <gipcompany@gmail.com>
2019-03-26 00:32:15 +0900

    async awaitを使用しない場合の処理を追加

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,7 +5,14 @@ export const READ_EVENTS = 'READ_EVENTS'
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
 
-export const readEvents = () => async dispatch => {
-  const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
-  dispatch({ type: READ_EVENTS, response })
+// export const readEvents = () => async dispatch => {
+//   const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+//   dispatch({ type: READ_EVENTS, response })
+// }
+
+export const readEvents = () => dispatch => {
+  axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
+    .then(response => {
+      dispatch({ type: READ_EVENTS, response })
+    })
 }
