87df943f41fee8f9fd0aa89b9a413fb32c818e09
Atsushi Ishida <gipcompany@gmail.com>
2019-08-04 12:40:17 +0900

    質問 #7797574 の回答

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,3 +8,21 @@ export const increment = () => ({
 export const decrement = () => ({
   type: DECREMENT
 })
+
+/* objectをreturnする場合 */
+const returnObjectPattern1 = () => { return { type: 'DECREMENT' } }
+const returnObjectPattern2 = () => ({ type: 'DECREMENT' })
+console.log(returnObjectPattern1())
+console.log(returnObjectPattern2())
+
+/* nullをreturnする場合 */
+const returnNullPattern1 = () => { return null }
+const returnNullPattern2 = () => (null)
+console.log(returnNullPattern1())
+console.log(returnNullPattern2())
+
+/* trueをreturnする場合 */
+const returnTruePattern1 = () => { return true }
+const returnTruePattern2 = () => (true)
+console.log(returnTruePattern1())
+console.log(returnTruePattern2())
