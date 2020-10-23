1dabf2087c8dcdc5f3943bb7830cbe6b08407f4c
Atsushi Ishida <gipcompany@gmail.com>
2020-10-23 22:50:01 +0900

    just make it prettier

--- src/App.js
+++ src/App.js
@@ -3,28 +3,30 @@ import PropTypes from 'prop-types';
 
 const App = () => {
   const profiles = [
-    { name: "Taro", age: 10 },
-    { name: "Hanako", age: 5 },
-    { name: "NoName", age: 3 }
-  ]
+    { name: 'Taro', age: 10 },
+    { name: 'Hanako', age: 5 },
+    { name: 'NoName', age: 3 },
+  ];
   return (
     <div>
-      {
-        profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index} />
-        })
-      }
+      {profiles.map((profile, index) => {
+        return <User name={profile.name} age={profile.age} key={index} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
 const User = (props) => {
-  return <div>Hi, I am {props.name}, and {props.age} years old! </div>
-}
+  return (
+    <div>
+      Hi, I am {props.name}, and {props.age} years old!{' '}
+    </div>
+  );
+};
 
 User.propTypes = {
   name: PropTypes.string,
-  age: PropTypes.number.isRequired
-}
+  age: PropTypes.number.isRequired,
+};
 
 export default App;
